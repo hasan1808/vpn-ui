2feade745a9fb33dfcb1dfa86f410c98b1c6350e
hasan1808 <55465985+hasan1808@users.noreply.github.com>
2026-08-26 18:55:27 +0330

    fix: re-add json_util import for sniffing RawMessage type

--- web/service/xray.go
+++ web/service/xray.go
@@ -13,6 +13,7 @@ import (
 	"github.com/mhsanaei/3x-ui/v2/database"
 	"github.com/mhsanaei/3x-ui/v2/database/model"
 	"github.com/mhsanaei/3x-ui/v2/logger"
+	"github.com/mhsanaei/3x-ui/v2/util/json_util"
 	"github.com/mhsanaei/3x-ui/v2/xray"
 
 	"go.uber.org/atomic"
